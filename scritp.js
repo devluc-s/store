5101c895ac2bd9312583198688f54bca6c0d4e7b
devluc-s <85721650+devluc-s@users.noreply.github.com>
2022-06-17 18:43:30 -0300

    Update scritp.js

--- scritp.js
+++ scritp.js
@@ -1,5 +1,5 @@
 let btn = document.querySelector('.btn-toggle button')
 let menu = document.querySelector('.menu-mobile')
-btn.addEventListener('click', ()=>[
+btn.addEventListener('click', ()=>{
   menu.classList.toggle('open')
-])
+})
