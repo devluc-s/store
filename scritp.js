fa5a017d0f947f9367c9fd970f898db01b91fc92
devluc-s <85721650+devluc-s@users.noreply.github.com>
2022-06-18 13:53:20 -0300

    Update scritp.js

--- scritp.js
+++ scritp.js
@@ -1,5 +1,13 @@
 let btn = document.querySelector('.btn-toggle button')
 let menu = document.querySelector('.menu-mobile')
+let overlay = document.querySelector('.bg-overlay')
 btn.addEventListener('click', ()=>{
-  menu.classList.toggle('open')
+  menu.classList.add('open')
+  document.body.classList.add('no-scroll')
+  if(menu.classList.contains('open') && document.body.classList.contains('no-scroll')){
+    overlay.addEventListener('click',()=>{
+      menu.classList.remove('open')
+      document.body.classList.remove('no-scroll')
+    })
+  }
 })
